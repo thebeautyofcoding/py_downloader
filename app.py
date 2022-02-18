fc42ea76eac6dcce7d7031c8a1c6ce057fc6e473
heiner <heiner.giehl@yahoo.com>
2022-02-18 14:35:42 +0100

    ...

---
+++ app.py
@@ -0,0 +1,195 @@
+# token.pickle stores the user's credentials from previously successful logins
+import pickle
+import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+import json
+from google.auth.transport.requests import Request
+if os.path.exists('token.pickle'):
+    print('Loading Credentials From File...')
+    with open('token.pickle', 'rb') as token:
+     
+      credentials = pickle.load(token)
+from googleapiclient.discovery import build
+import time
+import httplib2
+import random
+from urllib.error import HTTPError
+httplib2.RETRIES = 1
+from apiclient.http import MediaFileUpload
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+
+credentials=None
+
+
+
+# If there are no valid credentials available, then either refresh the token or log in.
+if not credentials or not credentials.valid:
+    if credentials and credentials.expired and credentials.refresh_token:
+        print('Refreshing Access Token...')
+        credentials.refresh(Request())
+    else:
+        print('Fetching New Tokens...')
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'client_secrets.json',
+            scopes=[
+                "https://www.googleapis.com/auth/youtube.upload"
+            ]
+        )
+
+        flow.run_local_server(port=3000, prompt='consent',
+                              authorization_prompt_message='')
+        credentials = flow.credentials
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as f:
+            print('Saving Credentials for Future Use...')
+            pickle.dump(credentials, f)
+
+VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
+
+
+def resumable_upload(insert_request):
+  print('RESUMMME')
+  response = None
+  error = None
+  retry = 0
+  while response is None:
+    try:
+      print( "Uploading file...")
+      status, response = insert_request.next_chunk()
+      if response is not None:
+        if 'id' in response:
+          print ("Video id '%s' was successfully uploaded." % response['id'])
+        else:
+          exit("The upload failed with an unexpected response: %s" % response)
+    except HttpError as e:
+      if e.resp.status in RETRIABLE_STATUS_CODES:
+        error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+                                                             e.content)
+      else:
+        raise
+    except RETRIABLE_EXCEPTIONS as e:
+      error = "A retriable error occurred: %s" % e
+
+    if error is not None:
+      print (error)
+      retry += 1
+      if retry > MAX_RETRIES:
+        exit("No longer attempting to retry.")
+
+      max_sleep = 2 ** retry
+      sleep_seconds = random.random() * max_sleep
+      print ("Sleeping %f seconds and then retrying..." % sleep_seconds)
+      time.sleep(sleep_seconds)
+      
+      
+def initialize_upload(youtube, video):
+     
+      tags = None
+      if video.keywords:
+        tags = video.keywords.split(",")
+
+      body=dict(
+        snippet=dict(
+          title=video.title,
+          description=video.description,
+          tags=tags,
+          category=video.categoryId
+        ),
+        status=dict(
+          privacyStatus=video.privacyStatus
+        )
+      )
+      # Call the API's videos.insert method to create and upload the video.
+      insert_request = youtube.videos().insert(
+        part=",".join(body.keys()),
+        body=body,
+        # The chunksize parameter specifies the size of each chunk of data, in
+        # bytes, that will be uploaded at a time. Set a higher value for
+        # reliable connections as fewer chunks lead to faster uploads. Set a lower
+        # value for better recovery on less reliable connections.
+        #
+        # Setting "chunksize" equal to -1 in the code below means that the entire
+        # file will be uploaded in a single HTTP request. (If the upload fails,
+        # it will still be retried where it left off.) This is usually a best
+        # practice, but if you're using Python older than 2.6 or if you're
+        # running on App Engine, you should set the chunksize to something like
+        # 1024 * 1024 (1 megabyte).
+        media_body=MediaFileUpload(video.filename, chunksize=-1, resumable=True)
+      )
+      resumable_upload(insert_request)
+        
+
+
+if __name__ == '__main__':
+    
+#   argparser.add_argument("--file", required=True, help="Video file to upload")
+#   argparser.add_argument("--title", help="Video title", default="Test Title")
+#   argparser.add_argument("--description", help="Video description",
+#     default="Test Description")
+#   argparser.add_argument("--category", default="22",
+#     help="Numeric video category. " +
+#       "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
+#   argparser.add_argument("--keywords", help="Video keywords, comma separated",
+#     default="")
+#   argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
+#     default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
+#   args = argparser.parse_args()
+
+#   if not os.path.exists(args.file):
+#     exit("Please specify a valid file using the --file= parameter.")
+ 
+    class VideoToUpload:
+        def __init__(self, title, filename):
+            self.title=title
+            self.description="test description"
+            self.privacyStatus=VALID_PRIVACY_STATUSES[1]
+            self.categoryId="22",
+            self.keywords=''
+            self.filename=filename
+            
+    directory = os.fsencode('processed')
+    filenames=list()
+    data=None
+    videoToUpload=None
+    videosToUpload=list()
+    def getVideoMetaData():
+        print('IAM RUNNING')
+        keys= list()
+        
+        with open('videoTitles.json', 'r') as f:
+            data=json.load(f)
+            keys= data.keys()
+            
+            
+        for file in os.listdir(directory):
+            filenames.append(os.fsdecode(file))
+        
+        for file in filenames:
+            
+            for key in keys:
+                if key in file:
+                    print(file)
+                    videoToUpload= VideoToUpload(data[key], f"processed/{file}")
+                    
+                    videosToUpload.append(videoToUpload)
+    getVideoMetaData()
+    
+    # youtube= build('youtube', 'v3', credentials=credentials)
+    # # app.run(host='localhost', port=3000)1
+    # for vid in videosToUpload:
+    #   try:
+    #       initialize_upload(youtube, vid)
+    #   except HTTPError as e:
+    #       print ("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)) 
+      
--- fb_dl.py
+++ fb_dl.py
@@ -1,9 +1,12 @@
 from asyncio.windows_events import NULL
+from msilib.schema import Error
+import shutil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from youtube_dl.utils import UnsupportedError, DownloadError
 import os
 import json
 import youtube_dl
@@ -13,6 +16,8 @@ import re
 import sys
 import argparse
 import requests
+import time
+import os.path
 chrome_options = Options()
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("--disable-infobars")
@@ -21,8 +26,8 @@ chrome_options.add_argument("--disable-popup-blocking")
 
 # disable the banner "Chrome is being controlled by automated test software"
 chrome_options.add_experimental_option("useAutomationExtension", False)
-chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
-url=input('Facebook-Page: ')
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+url=input("Facebook-Page: ")
 # global driver
 driver = webdriver.Chrome( options=chrome_options)
 
@@ -30,14 +35,28 @@ driver.get(url)
 
 button=WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Alle Cookies gestatten']"))).click()
 
+driver.implicitly_wait(10)
+links=[]
+links_videos_dl=[]
+titles=[]
 
-links = [elem.get_attribute("href") for elem in driver.find_elements(By.TAG_NAME,'a')]
+# links = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH,"//a")]
+# titles=[elem.get_attribute("textContent") for elem in driver.find_elements(By.XPATH,"//a/span/span")]    
+
+
+SCROLL_PAUSE_TIME = 1
+
+# Get scroll height
+
+
+  
 videoLinks=set()
-for link in links:
-    if '/videos/' in link:
-        videoLinks.add(link)
 
-print('VIDEOLINKS',videoLinks)
+# for link in links:
+#         if "videos" in link:
+#             videoLinks.add(link)
+# print('VIDEOLINKS', videoLinks)
+
 # excList= ["https://www.facebook.com/NyjahHustonSkate/videos/rome-was-tighttt-next-stop-olympics/4035254959901270/", "https://www.facebook.com/NyjahHustonSkate/videos/yesterdays-send/1401324856907350/"]
 
 
@@ -47,57 +66,109 @@ info=dict()
 videoTitleMapping=dict()
 
 
-def createVideoTitleMapping(slug, index):
-   
-    videoTitleMapping[index]=slug
- 
-    jsonVideoTitleMapping=json.dumps(videoTitleMapping, indent=4)
-    with open("videoTitles.json", "w") as outfile:
-        outfile.write(jsonVideoTitleMapping)
+# def createVideoTitleMapping(title, index):
+    
+    
+    
         
-def extractTitle(info):
-       webpage_url=info['webpage_url']
-       slug=webpage_url.split('/')[5]
-       return slug
-     
 
-for index, videoLink in enumerate(videoLinks):
-    input_audio=NULL
-    input_video=NULL
-    ydl_opts = {'format':'bestvideo/best','outtmpl':'{}.%(ext)s'.format(index)}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([videoLink])
-        info= ydl.extract_info(videoLink, download=False, process=True)
-        print('INFO', info)
-        slug=extractTitle(info)
-        createVideoTitleMapping(slug, index)
-        if os.path.isfile('./{}.mp4'.format(index)):
-            input_video = ffmpeg.input('./{}.mp4'.format(index))
-        else:
-            input_video = ffmpeg.input('./{}.webm'.format(index))
-        
-        
-    ydl_opts = {'format':'bestaudio/best','outtmpl':'{}.%(ext)s'.format(index)}
+
+while True:
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    
+      
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+    time.sleep(SCROLL_PAUSE_TIME)
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
    
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([videoLink])
-        info= ydl.extract_info(videoLink, download=False, process=True)
+    
+    
+    if new_height == last_height:
+        
+        break
+    last_height = new_height
+    
+
+links = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH,"//a")]
+titles=[elem.get_attribute("textContent") for elem in driver.find_elements(By.XPATH,"//a/span/span")]
+for link in links:
+    print(link)
+    if link:
+        if url in link:
+            videoLinks.add(link)
+    
      
-        input_audio = ffmpeg.input('./{}.m4a'.format(index))
+print("videoLINKS", videoLinks)
+for title in titles: 
+    
+    for index, videoLink in enumerate(videoLinks):
+        input_video=None
+        input_audio=None
+    
+    
+        ydl_opts = {"format":"bestvideo/best","outtmpl":"%(id)s.%(ext)s", "ignoreerrors":True}
+        try:    
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                
+                ydl.download([videoLink])
+                
+                info= ydl.extract_info(videoLink, download=False, process=True)
+            
+                print('INFO!!!', info['id'])
+                videoTitleMapping[info['id']]=title
+                
+                jsonVideoTitleMapping=json.dumps(videoTitleMapping, indent=4)
+                
+
+                    
+                    
+                if os.path.isfile(f"{info['id']}.mp4"):
+                    input_video = ffmpeg.input(f"{info['id']}.mp4")
+                else:
+                    input_video = ffmpeg.input(f"{info['id']}.webm")
+                        
+                        
+            ydl_opts = {"format":"bestaudio/best","outtmpl":"%(id)s.%(ext)s", "ignoreerrors":True}
+
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([videoLink])
+                print(videoLink)
+                info= ydl.extract_info(videoLink, download=False, process=True)
+                
+                if os.path.isfile(f"{info['id']}.m4a"):
+                    input_audio = ffmpeg.input(f"{info['id']}.m4a")
+                
+                
+            
+                if os.path.isfile(f"./processed/final_{info['id']}.mp4"):
+                    print("File already downloaded")
+                else:
+                    if input_audio==None:
+                        if os.path.isfile(f"{info['id']}.webm"):
+                            shutil.move(f"{info['id']}.webm", f"./processed/final_{info['id']}.webm")
+                        else:
+                            shutil.move(f"{info['id']}.mp4", f"./processed/final_{info['id']}.mp4")
+                    else:
+                        if input_video and input_audio:    
+                            ffmpeg.concat(input_video, input_audio, v=1, a=1).output(f"./processed/final_{info['id']}.mp4").run()
+        except:
+            print('failed')  
+    titles.remove(title)
+
+with open("videoTitles.json", "w") as outfile:
+    outfile.write(jsonVideoTitleMapping)       
+    
         
-        
-       
-    if os.path.isfile('./processed/final_{}.mp4'.format(index)):
-        print('File already downloaded')
-    else:
-        
-        ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./processed/final_{}.mp4'.format(index)).run()
 # driver.implicitly_wait(2)
-# # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-# username_box = driver.find_element_by_xpath('//*[@id ="email"]')
-# username_box.send_keys('heinergiehl2005@yahoo.de')
+# # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type="submit"]"))).click()
+# username_box = driver.find_element_by_xpath("//*[@id ="email"]")
+# username_box.send_keys("heinergiehl2005@yahoo.de")
 
-# password_box = driver.find_element_by_xpath('//*[@id ="pass"]')
-# password_box.send_keys('Heiner123@')
+# password_box = driver.find_element_by_xpath("//*[@id ="pass"]")
+# password_box.send_keys("Heiner123@")
 
 # WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.NAME, "login"))).click()
