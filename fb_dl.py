f80b633f2025fc2fe2aa1962f4183d8d76dfdb60
heiner <heiner.giehl@yahoo.com>
2022-02-19 22:56:45 +0100

    added main parts of  tiktok dl functionality

--- fb_dl.py
+++ fb_dl.py
@@ -40,37 +40,15 @@ links=[]
 links_videos_dl=[]
 titles=[]
 
-# links = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH,"//a")]
-# titles=[elem.get_attribute("textContent") for elem in driver.find_elements(By.XPATH,"//a/span/span")]    
 
 
 SCROLL_PAUSE_TIME = 1
-
-# Get scroll height
-
-
   
 videoLinks=set()
-
-# for link in links:
-#         if "videos" in link:
-#             videoLinks.add(link)
-# print('VIDEOLINKS', videoLinks)
-
-# excList= ["https://www.facebook.com/NyjahHustonSkate/videos/rome-was-tighttt-next-stop-olympics/4035254959901270/", "https://www.facebook.com/NyjahHustonSkate/videos/yesterdays-send/1401324856907350/"]
-
-
-   
+videoTitles=set()
 
 info=dict()
 videoTitleMapping=dict()
-
-
-# def createVideoTitleMapping(title, index):
-    
-    
-    
-        
 
 
 while True:
@@ -93,82 +71,87 @@ while True:
     last_height = new_height
     
 
-links = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH,"//a")]
-titles=[elem.get_attribute("textContent") for elem in driver.find_elements(By.XPATH,"//a/span/span")]
-for link in links:
-    print(link)
-    if link:
+links = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH,"//span/div/a")]
+titles=[elem.get_attribute("textContent") for elem in driver.find_elements(By.XPATH,"//span/div/a/span/span")]
+for index,link in enumerate(links):
+   
+    if link and index < 3:
         if url in link:
             videoLinks.add(link)
     
+for index,title in enumerate(titles):
+    if index < 3:
+     videoTitles.add(title)
      
-print("videoLINKS", videoLinks)
-for title in titles: 
-    
-    for index, videoLink in enumerate(videoLinks):
-        input_video=None
-        input_audio=None
-    
-    
-        ydl_opts = {"format":"bestvideo/best","outtmpl":"%(id)s.%(ext)s", "ignoreerrors":True}
-        try:    
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                
-                ydl.download([videoLink])
-                
-                info= ydl.extract_info(videoLink, download=False, process=True)
+print("LÄNGELINKS", len(videoLinks))
+print('LÄNGETITLE', len(videoTitles))
+jsonVideoTitleMapping=''
+videoTitleMapping=dict()
+zipped=zip(videoLinks, videoTitles)
+for videoLink, title in zipped:
+    input_video=None
+    input_audio=None
+    # if index+1==len(videoLinks):
+    #     break
+    print('videoLInksLoop')
+    ydl_opts = {"format":"bestvideo/best","outtmpl":"%(id)s.%(ext)s", "ignoreerrors":True}
+    try:
             
-                print('INFO!!!', info['id'])
-                videoTitleMapping[info['id']]=title
-                
-                jsonVideoTitleMapping=json.dumps(videoTitleMapping, indent=4)
-                
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            
+            ydl.download([videoLink])
+            
+            info= ydl.extract_info(videoLink, download=False, process=True)
+        
+            print('INFO!!!', info['id'])
+            videoTitleMapping[info['id']]=title
+            
+            
+            
 
+                
+                
+            if os.path.isfile(f"{info['id']}.mp4"):
+                input_video = ffmpeg.input(f"{info['id']}.mp4")
+            else:
+                input_video = ffmpeg.input(f"{info['id']}.webm")
                     
                     
-                if os.path.isfile(f"{info['id']}.mp4"):
-                    input_video = ffmpeg.input(f"{info['id']}.mp4")
-                else:
-                    input_video = ffmpeg.input(f"{info['id']}.webm")
-                        
-                        
-            ydl_opts = {"format":"bestaudio/best","outtmpl":"%(id)s.%(ext)s", "ignoreerrors":True}
+        ydl_opts = {"format":"bestaudio/best","outtmpl":"%(id)s.%(ext)s", "ignoreerrors":True}
 
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([videoLink])
-                print(videoLink)
-                info= ydl.extract_info(videoLink, download=False, process=True)
-                
-                if os.path.isfile(f"{info['id']}.m4a"):
-                    input_audio = ffmpeg.input(f"{info['id']}.m4a")
-                
-                
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([videoLink])
+            print(videoLink)
+            info= ydl.extract_info(videoLink, download=False, process=True)
             
-                if os.path.isfile(f"./processed/final_{info['id']}.mp4"):
-                    print("File already downloaded")
-                else:
-                    if input_audio==None:
-                        if os.path.isfile(f"{info['id']}.webm"):
-                            shutil.move(f"{info['id']}.webm", f"./processed/final_{info['id']}.webm")
-                        else:
-                            shutil.move(f"{info['id']}.mp4", f"./processed/final_{info['id']}.mp4")
+            if os.path.isfile(f"{info['id']}.m4a"):
+                input_audio = ffmpeg.input(f"{info['id']}.m4a")
+            
+            
+        
+            if os.path.isfile(f"./processed/final_{info['id']}.mp4"):
+                print("File already downloaded")
+            else:
+                if input_audio==None:
+                    if os.path.isfile(f"{info['id']}.webm"):
+                        shutil.move(f"{info['id']}.webm", f"./processed/final_{info['id']}.webm")
                     else:
-                        if input_video and input_audio:    
-                            ffmpeg.concat(input_video, input_audio, v=1, a=1).output(f"./processed/final_{info['id']}.mp4").run()
-        except:
-            print('failed')  
-    titles.remove(title)
+                        shutil.move(f"{info['id']}.mp4", f"./processed/final_{info['id']}.mp4")
+                else:
+                    print('prossing')
+                    if input_video and input_audio:    
+                        ffmpeg.concat(input_video, input_audio, v=1, a=1).output(f"./processed/final_{info['id']}.mp4").run()
+    except:
+        print('failed')
+        
+        
+    finally:
+        print('Done')
+         
+  
 
-with open("videoTitles.json", "w") as outfile:
-    outfile.write(jsonVideoTitleMapping)       
+
+with open("videoTitles.json", "w") as file:
+    json.dump(videoTitleMapping, file)       
     
         
-# driver.implicitly_wait(2)
-# # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type="submit"]"))).click()
-# username_box = driver.find_element_by_xpath("//*[@id ="email"]")
-# username_box.send_keys("heinergiehl2005@yahoo.de")
-
-# password_box = driver.find_element_by_xpath("//*[@id ="pass"]")
-# password_box.send_keys("Heiner123@")
-
-# WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.NAME, "login"))).click()
---
+++ tik_dl.py
@@ -0,0 +1,72 @@
+from TikTokApi import TikTokApi
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import ssl
+import shutil
+import json
+import os
+import urllib.request
+context = ssl._create_unverified_context()
+chrome_options = Options()
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-popup-blocking")
+
+
+
+
+# url=input("TikTok-Page: ")
+# SCROLL_PAUSE_TIME = 1
+
+# driver = webdriver.Chrome(options=chrome_options)
+# driver.implicitly_wait(10)
+# driver.get(url)
+verifyFP='verify_71a37c19940122ac0989cadd0e62bf3c'
+api = TikTokApi(custom_verify_fp=verifyFP, use_test_endpoints=True)
+
+# while True:
+#     last_height = driver.execute_script("return document.body.scrollHeight")
+    
+      
+#     # Scroll down to bottom
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+#     time.sleep(SCROLL_PAUSE_TIME)
+#     # Calculate new scroll height and compare with last scroll height
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+   
+    
+    
+#     if new_height == last_height:
+        
+#         break
+#     last_height = new_height
+
+
+user = api.user(username='danyellebutlercoach')
+user.as_dict # -> dict of the user_object
+# print(user.as_dict)
+# print(user.videos())
+videos= user.videos()
+videos=list(videos)
+video=videos[1]
+vidDic=video.as_dict
+vidCount=vidDic['authorStats']['videoCount']
+vidTitleMapping=dict()
+if int(vidCount) >1:
+    for index, video in enumerate(user.videos(count=int(30))):
+            vidDic=video.as_dict
+            print(vidDic['desc'])
+            dlurl=vidDic['video']['downloadAddr']
+            with urllib.request.urlopen(dlurl, context=context) as response:
+                with open(f"{vidDic['id']}.mp4", 'wb') as tmp_file:
+                    vidTitleMapping[vidDic['id']]=vidDic['desc']
+                    shutil.copyfileobj(response,tmp_file)
+                shutil.move(f"{vidDic['id']}.mp4", f"processed/{vidDic['id']}.mp4")
+                    
+                    
+with open("videoTitles.json", "w") as file:
+    json.dump(vidTitleMapping, file)     
